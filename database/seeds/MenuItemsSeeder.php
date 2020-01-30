68739d518f952e951776bd40af48af90e472916e
Md Abu Ahsan Basir <maab.career@gmail.com>
2020-01-30 15:09:24 +0600

    Updated MenuItem seeder

--- database/seeds/MenuItemsSeeder.php
+++ database/seeds/MenuItemsSeeder.php
@@ -14,27 +14,31 @@ class MenuItemsSeeder extends Seeder
      */
     public function run()
     {
-        $menu        = new Menu;
-        $menu->name  = "Admin";
-        $menu->slug  = Str::slug('Admin');
-        $menu->url   = '/admin';
-        $menu->order = 1;
-        $menu->save();
-
-        $menuItem               = new MenuItem;
-        $menuItem->menu_id      = $menu->id;
-        $menuItem->title        = "Menu Builder";
-        $menuItem->slug         = Str::slug("Menu Builder");
-        $menuItem->url          = "/admin/menus";
-        $menuItem->parent_id    = null;
-        $menuItem->order        = 1;
-        $menuItem->route        = 'admin.menus';
-        $menuItem->params       = null;
-        $menuItem->middleware   = null;
-        $menuItem->controller   = '\CodexShaper\Menu\Http\Controllers\MenuController@index';
-        $menuItem->target       = '_self';
-        $menuItem->icon         = null;
-        $menuItem->custom_class = null;
-        $menuItem->save();
+        if(! $menu = Menu::where('slug', 'admin')->first()) {
+            $menu        = new Menu;
+            $menu->name  = "Admin";
+            $menu->slug  = Str::slug('Admin');
+            $menu->url   = '/admin';
+            $menu->order = 1;
+            $menu->save();
+        }
+        if (! MenuItem::where('slug', Str::slug("Menu Builder"))->first()) {
+            $menuItem               = new MenuItem;
+            $menuItem->menu_id      = $menu->id;
+            $menuItem->title        = "Menu Builder";
+            $menuItem->slug         = Str::slug("Menu Builder");
+            $menuItem->url          = "/admin/menus";
+            $menuItem->parent_id    = null;
+            $menuItem->order        = 1;
+            $menuItem->route        = null;
+            $menuItem->params       = null;
+            $menuItem->middleware   = null;
+            $menuItem->controller   = '\CodexShaper\Menu\Http\Controllers\MenuController@index';
+            $menuItem->target       = '_self';
+            $menuItem->icon         = null;
+            $menuItem->custom_class = null;
+            $menuItem->save();
+        }
+        
     }
 }
